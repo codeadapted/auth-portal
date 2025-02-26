8a2b766e398314b5c8eae7a75531c232934bf347
thewebdreamer1 <thewebdreamer@gmail.com>
2025-02-25 20:52:58 -0600

    Revert "fix for env"
    This reverts commit 2662d1941a718f2c862b2e646757fab4976af3b2.

--- frontend/src/utils/adminUtils.js
+++ frontend/src/utils/adminUtils.js
@@ -1,6 +1,3 @@
-// Required modules
-require( 'dotenv' ).config();
-
 // Define the API URL, allowing for environment-specific flexibility
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
 
